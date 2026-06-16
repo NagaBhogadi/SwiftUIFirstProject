772866f7fd2b34687e6f9471f0d45fc54e5aa3eb
BNR <nrajithabhogadi@gmail.com>
2026-06-15 19:50:47 -0500

    added header and some color changes

--- SwiftUIFirstProject/3*3Matrix.swift
+++ SwiftUIFirstProject/3*3Matrix.swift
@@ -9,14 +9,18 @@ import SwiftUI
 
 struct __3Matrix: View {
     var body: some View {
-        VStack(spacing: 50){
-            HStack(spacing: 50){
-               
+    
+       VStack(spacing: 50){
+           Text("3*3 Matrix")
+               .bold()
+               .foregroundColor(.yellow)
+               .font(.system(size: 50))
+               .padding(.bottom,100)
+           HStack(spacing: 50){
+               Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
                 Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                
-            }
+                }
             .foregroundColor(.blue)
             HStack(spacing: 50){
                 Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
@@ -31,6 +35,8 @@ struct __3Matrix: View {
                 }
             .foregroundColor(.green)
         }
+       .frame(maxWidth: .infinity,maxHeight: .infinity)
+       .background(.brown)
 //        .foregroundColor(.yellow)
         
     }
