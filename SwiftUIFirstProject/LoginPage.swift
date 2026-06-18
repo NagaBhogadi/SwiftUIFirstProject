d426243507d52a29e50b2f0474ebdc46b173ec9c
BNR <nrajithabhogadi@gmail.com>
2026-06-18 14:23:15 -0500

    - Added @State variables for username and password - Changed password input to SecureField - Improved login screen layout using VStack spacing and padding - Added user icon at the top - Styled title with blue color, bold font, and larger size - Updated Login button with full width, background color, corner radius, and white text - Added Forgot Password text

--- SwiftUIFirstProject/LoginPage.swift
+++ SwiftUIFirstProject/LoginPage.swift
@@ -8,22 +8,49 @@
 import SwiftUI
 
 struct LoginPage: View {
+    @State var userName:String = ""
+    @State var password:String = ""
+    
     var body: some View {
-        VStack(spacing: 30){
-            Text("LogIn Detailes")
+        VStack(spacing: 20){
+            Image(systemName: "person.crop.circle")
+                .foregroundColor(Color.blue)
+                .font(.system(size: 50))
+            
+            Text("Login Deatiles")
                 .foregroundColor(.blue)
                 .font(.system(size: 30))
-            TextField("Enter User Name",text:.constant(""))
+                .fontWeight(.bold)
+            Spacer()
+            
+            TextField("Enter User Name",text: $userName)
                 .textFieldStyle(.roundedBorder)
-            TextField("Enter Password", text:.constant(""))
+                .font(.system(size: 25))
+            Spacer()
+            
+            TextField("Enter Password", text:$password)
                 .textFieldStyle(.roundedBorder)
-            Button("Login"){
+                .font(.system(size: 25))
+            Spacer()
+            
+            Button {
                 print("Tapped login")
+            } label: {
+                Text("Login")
+                    .foregroundColor(Color.blue)
+                    .font(.system(size: 25))
+            }
+            Button {
+                print(" Tapped Forget Password")
+            } label: {
+                Text("Forget Password?")
+                    .foregroundColor(Color.blue)
+                    .font(.system(size: 20))
             }
         }
-        .frame(maxWidth: .infinity,maxHeight: .infinity)
-        .padding(100)
-       }
+        .frame(maxWidth: 300,maxHeight: 300)
+        
+    }
 }
 
 #Preview {
